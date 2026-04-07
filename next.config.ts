cab1fef020adfd776ee7aae24d471e766c83190b
Utsab-crompoide <khatiwodautsab22@gmail.com>
2026-04-07 22:12:07 +0545

    minor change

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/train-brain", // Replace with your repo name
 };
 
 export default nextConfig;
