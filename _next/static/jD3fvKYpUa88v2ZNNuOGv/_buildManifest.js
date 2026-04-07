4c0a608ecfea1417b1e490a60aae7138d340c338
Utsab-crompoide <Utsab-crompoide@users.noreply.github.com>
2026-04-07 16:56:57 +0000

    deploy: 4c236af5cc1d922e1b990c3b712263fef80bbad0

--- _next/static/jD3fvKYpUa88v2ZNNuOGv/_buildManifest.js
+++ _next/static/jD3fvKYpUa88v2ZNNuOGv/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/train-brain//_next/:path+",
+        "destination": "/train-brain/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
